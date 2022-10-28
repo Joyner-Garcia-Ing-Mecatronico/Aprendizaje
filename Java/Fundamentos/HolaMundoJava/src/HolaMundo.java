dfd24c100e33549a14b04a18896f53c1570175f3
Joyner Garcia <ing.joynergarcia@gmail.com>
2022-10-28 11:15:43 -0500

    carpeta java

---
+++ Java/Fundamentos/HolaMundoJava/src/HolaMundo.java
@@ -0,0 +1,9 @@
+//Primer programa en Java
+public class HolaMundo {
+    
+    public static void main(String args[]){
+        
+        System.out.println("Hola Mundo desde Java");//Para imprimir
+}
+    
+}
