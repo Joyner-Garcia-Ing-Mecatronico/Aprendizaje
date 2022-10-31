85fe942e3ef5948924074433d672e5ed3d1e75df
Joyner Garcia <ing.joynergarcia@gmail.com>
2022-10-30 21:01:34 -0500

    Carga reto2 con cantidad de boletas y dinero total realizados, se hace por si se va la luz

--- Java/Clase_Mintic/Reto2/src/main/java/com/mycompany/reto2/Boleta.java
+++ Java/Clase_Mintic/Reto2/src/main/java/com/mycompany/reto2/Boleta.java
@@ -2,7 +2,12 @@ package com.mycompany.reto2;
 
 public class Boleta {
         //ATRIBUTOS
-    
+    String id;
+    String nombrePelicula;
+    String tipoBoleta;
+    int costoBoleta;
+    String documentoIdentidad;
+    String nombreCliente;
     
     
     //CONSTRUCTOR
@@ -17,50 +22,50 @@ public class Boleta {
     
     //GETTERS Y SETTERS (COMPLETAR)
     public String getNombreCliente() {
-        
+       return nombreCliente;
     }
 
     public void setNombreCliente(String nombreCliente) {
-        
+        this.nombreCliente = nombreCliente;
     }
 
     public String getId() {
-        
+        return id;
     }
 
     public void setId(String id) {
-        
+        this.id = id;
     }
 
     public String getNombrePelicula() {
-        
+        return nombrePelicula;
     }
 
     public void setNombrePelicula(String nombrePelicula) {
-        
+        this.nombrePelicula = nombrePelicula;
     }
 
     public String getTipoBoleta() {
-        
+        return tipoBoleta;
     }
 
     public void setTipoBoleta(String tipoBoleta) {
-        
+        this.tipoBoleta = tipoBoleta;
     }
 
     public int getCostoBoleta() {
-        
+        return costoBoleta;
     }
 
     public void setCostoBoleta(int costoBoleta) {
-        
+        this.costoBoleta = costoBoleta;
     }
 
     public String getDocumentoIdentidad() {
-        
+        return documentoIdentidad;
     }
 
     public void setDocumentoIdentidad(String documentoIdentidad) {
-        
+        this.documentoIdentidad = documentoIdentidad;
     }
 }
--- Java/Clase_Mintic/Reto2/src/main/java/com/mycompany/reto2/Reto2.java
+++ Java/Clase_Mintic/Reto2/src/main/java/com/mycompany/reto2/Reto2.java
@@ -1,14 +1,63 @@
 package com.mycompany.reto2;
 import java.util.ArrayList;
 
+/*Cantidad total de boletas vendidas
+Cantidad total de dinero recaudado en una función
+Cantidad total de boletas general vendidas
+Cantidad total de dinero recaudado por boletas general
+Cantidad total de boletas VIP vendidas
+Cantidad total de dinero recaudado por boletas VIP
+*/
+
 public class Reto2 {
+    
+    public static ArrayList<Boleta> t1 = new ArrayList<>();
+    
+    public static void main(String[] args) {
+        
+        t1.add(new Boleta("000", "Cars 3", "VIP", 24000, "1063987245", "Mar Rivera"));
+        t1.add(new Boleta("001", "Cars 3", "VIP", 24000, "1063741243", " Johan 2"));
+        t1.add(new Boleta("002", "Cars 3", "general", 15000, "71588659", "Paula H"));
+        t1.add(new Boleta("003", "Cars 3", "general", 15000, "98547632", "Mauro"));
+        t1.add(new Boleta("004", "Cars 31", "general", 15000, "147896301", "David M"));
+        
+        statisticsCinema(t1);
+    }
+    
+            
+            
+            
+        
+
 
     public static int[] statisticsCinema(ArrayList <Boleta> funcion){
         //EN ESTE ESPACIO PONER SU LÓGICA
+        int [] resultado = new int[5];
+        
+        resultado[0] = cantidadBoletas(funcion);
+        
+        resultado[1] = dineroTotal(funcion);
         
         
-        
+        return resultado;
     }
+    
+    public static int cantidadBoletas(ArrayList <Boleta> numBoletas){
+        int cantiBoletas = numBoletas.size();
+        
+        return cantiBoletas;
+    }
+    
+    public static int dineroTotal(ArrayList <Boleta> numBoletas){
+        int dinero = 0;
+        
+        for(int i=0; i<numBoletas.size(); i++){
+            dinero = dinero + numBoletas.get(i).getCostoBoleta();
+        }
+        
+        return dinero;
+    }
+
     
     
 }
